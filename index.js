baeef1f3a101a5967a49eace1df77ced3aa542f0
Lisa Baird <lmbaird4@gmail.com>
2017-02-16 12:06:24 -0700

    add save btn functionality.  tightened up css.

--- index.js
+++ index.js
@@ -6,40 +6,67 @@ function IdeaObj(title, body, quality){
   this.quality = quality || 'swill';
 }
 
+function hideExample () {
+    $('idea-card-example').addClass(hide);
+  }
+// function transfer (){
+//   $(.save-btn).on('click', function(){
+//     var newText=$(".my-idea").val();
+//     var newDescript=$(".my-descript").val();
+//     $(.storedidea).text(newText);
+//     $(.storeddescript).text(newDescript);
+//   })
+// }
+
+
 $(".save").on('click',function() {
-    var newText=$(".my-idea").val();
-    var newDescript=$(".my-descript").val();
-    var ideaId= Date.now;
+    var newText= $(".my-idea").val();
+    var newDescript= $(".my-descript").val();
+
     var idea = new IdeaObj(newText, newDescript);
-  $(".idea-container").prepend(<article class = "idea-card">
-    <button class="delete" type='radio'>X</button>
-    <p class="stored-idea"></p>
-      <p class="stored-descript"></p>
-    <button class="up-btn" type ='radio'>U</button>
-    <button class="down-btn" type='radio'>D</button>
-    <p class="quality">Quality: Swill </p>
-  </article>)  
-    $(".stored-idea").text(newText);
-    $(".stored-descript").text(newDescript);
+    // $(.storedidea).text(newText);
+    // $(.storeddescript).text(newDescript);
+  $(".idea-container").prepend(`
+    <article id="${idea.id}" class="idea-card">
+      <button class="delete" type='radio'>X</button>
+      <p class="stored-idea">${idea.title}</p>
+      <p class="stored-descript">${idea.body}</p>
+      <button class="up-btn">U</button>
+      <button class="down-btn">D</button>
+      <p class="quality">Quality:<span class="choice">Swill</span></p>
+    </article>`);
+    // $(.storedidea).text(newText);
+    // $(.storeddescript).text(newDescript);
   });
 
-// Enable the U D buttons.  Use a conditional for the different choices of quality: swill, mediocre and genius.
+// $('.idea-container').on('click','.up-btn', function() {
+//     console.log(this);
+// $('li:contains("swill")').text("plausible");
+// $('li:contains("plausible")').text("genius");
+// });
+// // Enable the U D buttons.  Use a conditional for the different choices of quality: swill, mediocre and genius.
+// //
 //
-
-$('up-btn').on('click', function() {
-  var plausible = "plausible"
-  var genius = "genius"
-  if(".quality").text("swill").text(plausible)
-} else {
-  if(".quality").text("mediocre").text(genius)
-}
-})
-
-$('down-btn').on('click', function() {
-  var plausible = "plausible"
-  var genius = "genius"
-  var swill ="swill"
-  if(".quality").text("genius").text(mediocre)
-} else {
-  if(".quality").text("plausible").text(swill)
-}
+// $('.idea-container').on('click','.up-btn', function() {
+//   console.log(this);
+//     if ($(.choice).text(plausible).
+//   } else {
+//     if($(.choice).text()==("mediocre").text(genius)
+//   }
+// })
+//
+// // $('.up-btn').on('click', function() {
+// //   var plausible = "plausible"
+// //   var genius = "genius"
+// //   console.log('hi');
+// //
+// // })
+//
+// // $('down-btn').on('click', function() {
+// //   var plausible = "plausible"
+// //   var genius = "genius"
+// //   var swill ="swill"
+// //   if(".quality").text("genius").text(mediocre)
+// // } else {
+// //   if(".quality").text("plausible").text(swill)
+// // }
